0227cee854e101943d614f9ec5f9400feaac6146
Benjamin Kaganovitch <BKaganovit@smail.pembinatrails.ca>
2023-05-03 18:55:45 -0500

    Git Test

--- Dungeons & Dragons/Controllers/HomeController.cs
+++ Dungeons & Dragons/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace Dungeons___Dragons.Controllers
 {
+    //git test
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
