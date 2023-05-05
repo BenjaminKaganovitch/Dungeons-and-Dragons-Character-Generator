848f40976008cbaa9ca7e2154e3c3069f9454579
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-05-04 21:59:45 -0500

    Begin Domain Layer

---
+++ Dungeons & Dragons.Domain/Spell.cs
@@ -0,0 +1,18 @@
+﻿using Dungeons___Dragons.Models;
+
+namespace Dungeons___Dragons.Domain
+{
+    public class Spell
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Source { get; set; } = null!;
+        public string CreatorId { get; set; } = null!;
+        public UserLeadEntity Creator { get; set; } = null!;
+        public Type SpellType { get; set; }
+        public string MagicSchool { get; set; } = null!;
+        public string Components { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+    }
+}
---
+++ Dungeons & Dragons.Domain/Type.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons___Dragons.Domain
+{
+    public enum Type
+    {
+        Cantrip,
+        FirstLevel,
+        SecondLevel,
+        ThirdLevel,
+        FourthLevel,
+        FifthLevel,
+        SixthLevel,
+        SeventhLevel,
+        EighthLevel,
+        NinthLevel
+    }
+}
