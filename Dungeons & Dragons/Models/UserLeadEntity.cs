ef55f2e1c963a134620b126edbd8a5faef4fcc58
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-05-04 19:34:56 -0500

    Fix AddDefaultIdentity, but break Id-based code

--- Dungeons & Dragons/Models/UserLeadEntity.cs
+++ Dungeons & Dragons/Models/UserLeadEntity.cs
@@ -1,8 +1,10 @@
-﻿namespace Dungeons___Dragons.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Dungeons___Dragons.Models
 {
-    public class UserLeadEntity
+    public class UserLeadEntity : IdentityUser
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Mobile { get; set; }
