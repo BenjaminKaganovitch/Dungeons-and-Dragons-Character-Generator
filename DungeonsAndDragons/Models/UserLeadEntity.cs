4a116b7638a6842d843c0b2ff245cd8e372d8894
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-05-09 09:35:44 -0500

    Remove circular dependency

--- DungeonsAndDragons/Models/UserLeadEntity.cs
+++ DungeonsAndDragons/Models/UserLeadEntity.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Dungeons___Dragons.Domain;
 
 namespace Dungeons___Dragons.Models
 {
@@ -8,13 +7,6 @@ namespace Dungeons___Dragons.Models
 		public string FirstName { get; set; } = null!;
 		public string LastName { get; set; } = null!;
 		public string Mobile { get; set; } = null!;
-		public IEnumerable<Character> Characters { get; set; } = null!;
-		public IEnumerable<Feat> HomebrewFeats { get; set; } = null!;
-		public IEnumerable<Subclass> HomebrewSubclasses { get; set; } = null!;
-		public IEnumerable<Spell> HomebrewSpells { get; set; } = null!;
-		public IEnumerable<Background> HomebrewBackgrounds { get; set; } = null!;
-		public IEnumerable<Lineage> HomebrewLineages { get; set; } = null!;
-		public IEnumerable<Sublineage> HomebrewSublineages { get; set; } = null!;
 		public string AcceptedSources { get; set; } = null!;
 	}
 }
