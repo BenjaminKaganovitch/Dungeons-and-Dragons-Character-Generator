2215f922e5ce6cc4ba4e3c519c87aceaed705b76
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-05-09 09:15:18 -0500

    Edit UserLeadEntity

--- DungeonsAndDragons/Models/UserLeadEntity.cs
+++ DungeonsAndDragons/Models/UserLeadEntity.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Dungeons___Dragons.Domain;
 
 namespace Dungeons___Dragons.Models
 {
-    public class UserLeadEntity : IdentityUser
-    {
-        //public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Mobile { get; set; }
-        public string Email { get; set; }
-        public string Source { get; set; }
-    }
+	public class UserLeadEntity : IdentityUser
+	{
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string Mobile { get; set; } = null!;
+		public IEnumerable<Character> Characters { get; set; } = null!;
+		public IEnumerable<Feat> HomebrewFeats { get; set; } = null!;
+		public IEnumerable<Subclass> HomebrewSubclasses { get; set; } = null!;
+		public IEnumerable<Spell> HomebrewSpells { get; set; } = null!;
+		public IEnumerable<Background> HomebrewBackgrounds { get; set; } = null!;
+		public IEnumerable<Lineage> HomebrewLineages { get; set; } = null!;
+		public IEnumerable<Sublineage> HomebrewSublineages { get; set; } = null!;
+		public string AcceptedSources { get; set; } = null!;
+	}
 }
