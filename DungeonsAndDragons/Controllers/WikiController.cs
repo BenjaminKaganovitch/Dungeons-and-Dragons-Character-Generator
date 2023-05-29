5830eda9484f2f917a28dbfe55ab06615a46268a
Xander <aburnett@smail.pembinatrails.ca>
2023-05-29 09:44:28 -0500

    Added spell page and action

--- DungeonsAndDragons/Controllers/WikiController.cs
+++ DungeonsAndDragons/Controllers/WikiController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DungeonsAndDragons.DataAccess;
+using DungeonsAndDragons.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DungeonsAndDragons.Controllers
 {
 	public class WikiController : Controller
 	{
-		public IActionResult WikiHomePage()
+
+        private readonly ApplicationDbContext _db;
+
+        public WikiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult WikiHomePage()
 		{
 			return View();
+		}
+
+		public IActionResult WikiSpellPage(int id)
+		{
+			if(id == null)
+			{
+				return NotFound();
+			}
+			Spell spell = _db.Spells.Include(s=>s.Creator).FirstOrDefault(s => s.Id == id);
+			if(spell == null)
+			{
+				return NotFound();
+			}
+			return View(spell);
 		}
 	}
 }
