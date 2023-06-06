89d7d586f58187b801415d95ad289e5176ebfabd
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-06-05 21:20:58 -0500

    Minor tweak to SeedData

--- DungeonsAndDragons.Services/SeedData.cs
+++ DungeonsAndDragons.Services/SeedData.cs
@@ -410,7 +410,7 @@ namespace DungeonsAndDragons.Services
                                           "accustomed to a difficult life in rugged terrain. You’re " +
                                           "probably on the tall side (for a dwarf), and tend toward " +
                                           "lighter coloration. The shield dwarves of northern " +
-                                          "Faerûn, as well as the ruling Hylar clan and the noble" +
+                                          "Faerûn, as well as the ruling Hylar clan and the noble " +
                                           "Daewar clan of Dragonlance, are mountain dwarves.",
                             StatBoosts = new List<StatBoost>()
                             {
